66f8c37db840ff173ef62d236501c4b95c1b426d
scribblerp9 <szappia86@gmail.com>
2021-08-25 12:29:57 +1000

    Hero projects now display on home page

--- .eleventy.js
+++ .eleventy.js
@@ -46,6 +46,10 @@ module.exports = function (eleventyConfig) {
         return now;
     });
 
+    eleventyConfig.addFilter('log', value => {
+        console.log(value);
+    });
+
     // If being deployed (build rather than start), minify everything
     eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
         if (process.env.ELEVENTY_PRODUCTION && outputPath && outputPath.endsWith('.html')) {
